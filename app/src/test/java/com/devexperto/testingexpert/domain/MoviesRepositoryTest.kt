e178dd23b2a53c984b9f9a2b97e7af90bfe81a04
Manuel Montañés <montanes.manuel@gmail.com>
2024-06-07 22:19:20 +0200

    Use a spy as local data sourde

--- app/src/test/java/com/devexperto/testingexpert/domain/MoviesRepositoryTest.kt
+++ app/src/test/java/com/devexperto/testingexpert/domain/MoviesRepositoryTest.kt
@@ -10,12 +10,14 @@ class MoviesRemoteDataSourceStub: MoviesRemoteDataSource {
     )
 }
 
-class MoviesLocalDataSourceFake: MoviesLocalDataSource {
+class MoviesLocalDataSourceSpy: MoviesLocalDataSource {
     private val moviesDataSource = mutableListOf<Movie>()
+    var saveAllCalled = false
 
     override fun isEmpty(): Boolean = moviesDataSource.isEmpty()
 
     override fun saveAll(movies: List<Movie>) {
+        saveAllCalled = true
         moviesDataSource.addAll(movies)
     }
 
@@ -26,7 +28,7 @@ class MoviesRepositoryTest{
 
     @Test
     fun `by default use local data source if exists`(){
-        val moviesLocalDataSource = MoviesLocalDataSourceFake()
+        val moviesLocalDataSource = MoviesLocalDataSourceSpy()
         moviesLocalDataSource.saveAll(listOf(Movie(1, "Movie 1")))
         val moviesRemoteDataSource = MoviesRemoteDataSourceStub()
         val moviesRepository = MoviesRepository(moviesLocalDataSource, moviesRemoteDataSource)
@@ -38,12 +40,23 @@ class MoviesRepositoryTest{
 
     @Test
     fun `if local data source is empty, use remote data source`(){
-        val moviesLocalDataSource = MoviesLocalDataSourceFake()
+        val moviesLocalDataSource = MoviesLocalDataSourceSpy()
         val moviesRemoteDataSource = MoviesRemoteDataSourceStub()
         val moviesRepository = MoviesRepository(moviesLocalDataSource, moviesRemoteDataSource)
 
         val movies = moviesRepository.findAll()
 
         assertEquals(2, movies.size)
+    }
+
+    @Test
+    fun `if local data source is empty, save remote data source in local data source`(){
+        val moviesLocalDataSource = MoviesLocalDataSourceSpy()
+        val moviesRemoteDataSource = MoviesRemoteDataSourceStub()
+        val moviesRepository = MoviesRepository(moviesLocalDataSource, moviesRemoteDataSource)
+
+        moviesRepository.findAll()
+
+        assertEquals(true, moviesLocalDataSource.saveAllCalled)
     }
 }
