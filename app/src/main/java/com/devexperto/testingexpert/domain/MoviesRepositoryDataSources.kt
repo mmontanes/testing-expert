bfe362b8dee87f8a3e492fdac767418ee86d6dfc
Manuel Montañés <montanes.manuel@gmail.com>
2024-06-07 21:48:37 +0200

    Create fake repository data source

---
+++ app/src/main/java/com/devexperto/testingexpert/domain/MoviesRepositoryDataSources.kt
@@ -0,0 +1,28 @@
+package com.devexperto.testingexpert.domain
+
+data class Movie(val id: Int, val title: String)
+
+class MoviesRepository(
+    private val moviesLocalDataSource: MoviesLocalDataSource,
+    private val moviesRemoteDataSource: MoviesRemoteDataSource
+) {
+
+    fun findAll(): List<Movie> {
+        if (moviesLocalDataSource.isEmpty()) {
+            val movies = moviesRemoteDataSource.findPopularMovies()
+            moviesLocalDataSource.saveAll(movies)
+        }
+        return moviesLocalDataSource.findAll()
+    }
+
+}
+
+interface MoviesLocalDataSource {
+    fun isEmpty(): Boolean
+    fun saveAll(movies: List<Movie>)
+    fun findAll(): List<Movie>
+}
+
+interface MoviesRemoteDataSource {
+    fun findPopularMovies(): List<Movie>
+}
