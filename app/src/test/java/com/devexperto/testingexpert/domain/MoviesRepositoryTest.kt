4109a2918fe25892bb51e4f8a46989bb2d7334f7
Manuel Montañés <montanes.manuel@gmail.com>
2024-06-07 22:13:58 +0200

    Use more complex test doubles

--- app/src/test/java/com/devexperto/testingexpert/domain/MoviesRepositoryTest.kt
+++ app/src/test/java/com/devexperto/testingexpert/domain/MoviesRepositoryTest.kt
@@ -3,33 +3,47 @@ package com.devexperto.testingexpert.domain
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class MoviesRemoteDataSourceDummy: MoviesRemoteDataSource {
-    override fun findPopularMovies(): List<Movie> {
-        return emptyList()
-    }
+class MoviesRemoteDataSourceStub: MoviesRemoteDataSource {
+    override fun findPopularMovies(): List<Movie> = listOf(
+        Movie(1, "Movie 1"),
+        Movie(2, "Movie 2")
+    )
 }
 
-class MoviesLocalDataSourceStub: MoviesLocalDataSource {
-    override fun isEmpty(): Boolean = false
+class MoviesLocalDataSourceFake: MoviesLocalDataSource {
+    private val moviesDataSource = mutableListOf<Movie>()
+
+    override fun isEmpty(): Boolean = moviesDataSource.isEmpty()
 
     override fun saveAll(movies: List<Movie>) {
-        // TODO: Not necessary for this test
+        moviesDataSource.addAll(movies)
     }
 
-    override fun findAll(): List<Movie> = listOf(Movie(1, "The Shawshank Redemption"))
+    override fun findAll(): List<Movie> = moviesDataSource
 }
 
 class MoviesRepositoryTest{
 
     @Test
     fun `by default use local data source if exists`(){
-        val moviesLocalDataSource = MoviesLocalDataSourceStub()
-        val moviesRemoteDataSource = MoviesRemoteDataSourceDummy()
+        val moviesLocalDataSource = MoviesLocalDataSourceFake()
+        moviesLocalDataSource.saveAll(listOf(Movie(1, "Movie 1")))
+        val moviesRemoteDataSource = MoviesRemoteDataSourceStub()
         val moviesRepository = MoviesRepository(moviesLocalDataSource, moviesRemoteDataSource)
 
         val movies = moviesRepository.findAll()
 
-        assertEquals(1, movies[0].id)
+        assertEquals(1, movies.size)
     }
 
+    @Test
+    fun `if local data source is empty, use remote data source`(){
+        val moviesLocalDataSource = MoviesLocalDataSourceFake()
+        val moviesRemoteDataSource = MoviesRemoteDataSourceStub()
+        val moviesRepository = MoviesRepository(moviesLocalDataSource, moviesRemoteDataSource)
+
+        val movies = moviesRepository.findAll()
+
+        assertEquals(2, movies.size)
+    }
 }
